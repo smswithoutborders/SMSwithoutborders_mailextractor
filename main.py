e35d49ae5627167985874431b1df3a110073abfe
sh3rlock <wisdomnji@gmail.com>
2021-06-24 23:23:50 +0100

    updated: extracts messages payload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import imaplib
 import sys
 import os
 import configparser
+import email
 from email.parser import HeaderParser
 
 CONFIGS = configparser.ConfigParser(interpolation=None)
@@ -22,14 +23,16 @@ imap.select('Inbox')
 typ, msgnums = imap.search(None, 'ALL')
 
 HEADER_INDEX=1
+TEXT_INDEX=0
 parser = HeaderParser()
 for num in msgnums[0].split():
-    mtyp, msg = imap.fetch(num, '(BODY[HEADER])')
-    # print(type(msg[0][HEADER_INDEX]))
-    # msg = msg[0][1].decode('utf-8')
+    mtyp, msg = imap.fetch(num, '(RFC822)')
     header_data=msg[0][HEADER_INDEX]
-    msg = parser.parsestr(header_data.decode('utf-8'))
-    # print(msg.keys())
+    msg = email.message_from_bytes(header_data)
+    print(msg.get_charsets())
+    for part in msg.walk():
+        if part.get_content_type() == "text/plain":
+            print(part.get_payload())
     print(f"From {msg['From']}")
     print(f"To {msg['To']}")
     print(f"Subject {msg['Subject']}")
